154d47ddd069f36ed0b6257bfbb8d73f3275b635
biscayne-badger <shashanknavale@ufl.edu>
2023-07-19 13:45:11 -0400

    worked on decode() function and option 2 in the main function

--- main.py
+++ main.py
@@ -18,11 +18,29 @@ def encode(password):
     return new_password
 
 
-def decode():
-    pass
+def decode(password):
+    decoded = ""  # initializes an empty decoded string variable
+
+    for element in password:
+        if element == "0":
+            decoded += "7"
+
+        elif element == "1":
+            decoded += "8"
+
+        elif element == "2":
+            decoded += "9"
+        # above portion of code replaces necessary characters
+
+        else:
+            decoded += str(int(element) - 3)  # shifts down rest of the elements by 3
+
+    return decoded  # returns the decoded string
 
 
 def main():
+    global encoded_password
+    old_password = ""
     while True:
         # for option 1 I kept the old password and encoded password separate so that they can be called in option 2
         print()
@@ -30,16 +48,22 @@ def main():
         option = int(input("Please enter an option: "))
         if option == 1:
             print("Please enter your password to encode: ", end="")
-            old_password = input()
+            old_password += input()
             encoded_password = encode(old_password)
-            print(encoded_password)
             print("Your password has been encoded and stored!")
+
         elif option == 2:
-            pass
+            if len(old_password) == 0:  # checks if old password is empty
+                print("No password encoded yet. Please use Option 1.")  # prompts user to use Option 1
+                continue # continues the loop
+            decoded_password = decode(encoded_password)  # decodes the previously encoded password
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+
         elif option == 3:
-            break
+            break  # exits out of the program
+
         else:
-            print("This isn't a valid option! Please try again.\n")
+            print("This isn't a valid option! Please try again.\n")  # prompts user to enter valid option
 
 
 if __name__ == '__main__':
